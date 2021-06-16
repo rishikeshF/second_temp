c30164c1fb6e84f6d56c79c8e736d74564eefc2f
rishikesh <divyarishifulari@gmail.com>
2021-06-16 18:24:21 +0530

    404 feature implemented

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const http = require("http");
 
-http 
+http
     .createServer((req,res) => {
-        console.log(res);
-        res.write("<h1>Home Page!</h1>");
-        res.end();
-    })
-    .listen(8080)
+        switch (requestAnimationFrame.url) {
+            case "/":
+                Response.writeHead(200, {
+                    "Content-Type":"text/html",
+
+                });
+                Response.write("<h1>Home Page</h1>");
+                Response.end();
+                break;
+            default :
+            Response.writetoHead(404, {
+                "Content-Type":"text/html",
+
+            });
+            Response.write("<p>404 Page not found</p>");
+            Response.end();
+            break;
+        }
+    }
+    .listen(8080);
